9d02a422fda96d1c8d7bcb445a531100d7da368d
swethakrishnan <swethapt333@gmail.com>
2022-01-29 16:25:26 +0530

    initial commit

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -2,7 +2,8 @@
 
 const allowedOrigins = [
     'http://localhost:5500',
-    'http://127.0.0.1:5500'
+    'http://127.0.0.1:5500',
+    'https://swethakrishnan99.github.io/'
 ];
 
 module.exports = allowedOrigins
