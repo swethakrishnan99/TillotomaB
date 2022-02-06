698e4b40177e592fcb0b5921fb7ecb7cff0289e4
swethakrishnan <swethapt333@gmail.com>
2022-02-06 17:35:00 +0530

    added forgot password and reset password page

--- routes/forget.js
+++ routes/forget.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const router = express.Router();
 const resetController = require('../controllers/PasswordResetController/resetController')
+router.use(cors())
 
 router.put("/", resetController.forgotPassword)
 
